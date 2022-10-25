fe80fcba50dd55bf299c1f87dffbba496d7a12fd
Mickey <mickeyc11111@gmail.com>
2022-10-24 22:57:12 -0400

    commit tree 375e696cec71d64326de770b8f42139f64a24317

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    if @length1 <= 0 || @length2 <= 0 || @length3 <= 0
+      raise TriangleError
+    elsif @length1 + @length2 <= @length3 || @length1 + @length3 <= @length2 || @length2 + @length3 <= @length1
+      raise TriangleError
+    elsif @length1 == @length2 && @length2 == @length3
+      @kind = :equilateral
+    elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3
+      @kind = :isosceles 
+    else @length1 != @length2 && @length2 != @length3
+      @kind = :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      puts "Not a valid triangle!"
+    end
+  end
 end
